56b19ab0ba043d69af731aa74ad455707cdec145
Stephen Wolff <stephen@maxgatedigital.com>
2018-11-01 11:55:50 +0000

    Add some commenting

--- src/wnu_csv/wmu_csv.go
+++ src/wnu_csv/wmu_csv.go
@@ -10,14 +10,19 @@ import _ "github.com/go-sql-driver/mysql"
 
 func main() {
 	filePath := "/home/stephendwolff/foo"
+
+	// register file with MySQL so that it can be used with LOAD DATA LOCAL INFILE
 	mysql.RegisterLocalFile(filePath)
+
+	// prepare the database connection - it opens lazily when first used
 	db, dberr := sql.Open("mysql", "user:pass@/database_name")
+
 	if dberr != nil {
 		log.Println( fmt.Errorf("wnu_csv: dberr: %s", dberr))
 	} else {
-
+		// CSV file first line:
 		// "id","display_name","id","created_at"
-		result, err := db.Exec("LOAD DATA LOCAL INFILE '" + filePath + "' INTO TABLE classifications " +
+		_, err := db.Exec("LOAD DATA LOCAL INFILE '" + filePath + "' INTO TABLE classifications " +
 			"FIELDS TERMINATED BY ',' " +
 			"ENCLOSED BY '\"' " +
 			"IGNORE 1 LINES " +
@@ -25,9 +30,10 @@ func main() {
 		if err != nil {
 			log.Println( fmt.Errorf("wnu_csv: load_data err: %s", err))
 		} else {
-
-			log.Println( result )
+			log.Println( "Success" )
 		}
-
 	}
+
+	// be a good citizen
+	db.Close()
 }
